79b5e5a6f53150549acdcba4a476108208946da5
Paola82254 <teacherpaola4kids@gmail.com>
2023-10-26 17:32:09 -0300

    update-php

--- exercicio01.php
+++ exercicio01.php
@@ -5,7 +5,17 @@ Idade que esta pessoa tem (exemplo: 25)
 Mostre no HTML uma mensagem qualquer apresentando o nome da pessoa e a idade dela. Exemplo: "Chapolin possui 25 anos."
 
 Destaque o nome em itálico e a idade em negrito. -->
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 01 (corrigido)</title>
+</head>
+<body>
+    <h1>Exercício 01 (corrigido)</h1>
+</body>
+</html>
 <?php
 $nome = "Chapolin";
 $idade = 25;
