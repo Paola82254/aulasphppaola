009ee7c8c46ede2063f9fdedd7ab0de8f55c0e99
Paola82254 <teacherpaola4kids@gmail.com>
2023-11-07 15:24:10 -0300

    07-11

--- 04-condicionais-versao2.php
+++ 04-condicionais-versao2.php
@@ -23,9 +23,12 @@
     <h2>Simples(não tem else)</h2>
     <?php
     $numero= 100;
-
+    
     if($numero > 20){
-        echo "<p>$numero é maior que 20.</p>";
+    
+    ?>
+        <p><?=$numero?> é maior que 20.</p>
+    <?php
     }
     ?>
     <!-- if (se), else(senão) -->
@@ -40,23 +43,22 @@
 
     <h3>Produto: <?=$produto?></h3>
     <h4>Estoque: <?=$qtdeEmEstoque?></h3>
-    <!-- Se a quantidade em Estoque for abaixo da quantidade crítica, então o sistema deve avisar e pedir para repor. -->
+    
     <?php
-    if($qtdeEmEstoque < $qtdeCritica){
-        echo "<p class='repor'>É necessário comprar/repor!</p>";
+    if($qtdeEmEstoque < $qtdeCritica){ ?>
+        <p class="repor">É necessário comprar/repor!</p>
 
-        //Condicional SIMPLES/ANINHADA
-        if($qtdeEmEstoque === 0){
-            echo "<p class='urgente'>URGENTE</p>";
-        }
+        <?php    
+        if($qtdeEmEstoque === 0){ ?>
+            <p class="urgente">URGENTE</p>
+        <?php }
 
     }else {
-        /* Caso contrário, simplesmente mostrar que o estoque está normal */
-        echo "<p class='normal'>Estoque normal</p>";
-    }
     ?>
-    <!-- --------------------- -->
-    <?=
+         <p class="normal">Estoque normal</p>
+    <?php
+    }
+    
     $a = 5;
     $b = "5";
 
